20bd788da7a828eb08401c8f03762d71e4a463f4
Abdullah Alfallaj <afalfallaj@gmail.com>
2026-01-05 23:11:20 +0300

    feat: Bump version to 1.1.0 and import new Gimdow BLE device and data point components.

--- custom_components/gimdow_ble/gimdow_ble/__init__.py
+++ custom_components/gimdow_ble/gimdow_ble/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-__version__ = "1.0.2"
+from .const import GimdowBLEDataPointType, SERVICE_UUID
+from .gimdow_ble import GimdowBLEDataPoint, GimdowBLEDevice
+from .manager import AbstaractGimdowBLEDeviceManager, GimdowBLEDeviceCredentials
+
+__version__ = "1.1.0"
 
 __all__ = [
     "AbstaractGimdowBLEDeviceManager",
