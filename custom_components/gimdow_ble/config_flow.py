b32052f4a06ab248a13008a50f354d93b174b4bc
Abdullah Alfallaj <afalfallaj@gmail.com>
2026-01-13 17:18:15 +0300

    updated config flow

--- custom_components/gimdow_ble/config_flow.py
+++ custom_components/gimdow_ble/config_flow.py
@@ -179,10 +179,6 @@ def _show_login_form(
 class GimdowBLEOptionsFlow(OptionsFlowWithReload):
     """Handle a Gimdow BLE options flow."""
 
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
