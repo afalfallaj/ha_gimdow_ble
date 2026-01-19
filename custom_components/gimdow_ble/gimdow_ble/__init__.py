6bc272329241c1c4d05c0e8f3c0f04dc328d8d7f
Abdullah Alfallaj <afalfallaj@gmail.com>
2026-01-19 12:40:05 +0300

    chore: Bump version to 1.2.2.

--- custom_components/gimdow_ble/gimdow_ble/__init__.py
+++ custom_components/gimdow_ble/gimdow_ble/__init__.py
@@ -4,7 +4,7 @@ from .const import GimdowBLEDataPointType, SERVICE_UUID
 from .gimdow_ble import GimdowBLEDataPoint, GimdowBLEDevice
 from .manager import AbstaractGimdowBLEDeviceManager, GimdowBLEDeviceCredentials
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 __all__ = [
     "AbstaractGimdowBLEDeviceManager",
