67b22432c1da414e09c2c837c2b4af3429c22280
Abdullah Alfallaj <46138890+afalfallaj@users.noreply.github.com>
2026-07-25 01:48:21 +0300

    chore(main): release 3.3.5

--- custom_components/gimdow_ble/gimdow_ble/__init__.py
+++ custom_components/gimdow_ble/gimdow_ble/__init__.py
@@ -12,7 +12,7 @@ from .manager import AbstractGimdowBLEDeviceManager, GimdowBLEDeviceCredentials
 from .lock_manager import GimdowBLELockManager, LockBlockedReason, PendingLockIntent
 from .exceptions import GimdowBLEError
 
-__version__ = "3.3.4"  # x-release-please-version
+__version__ = "3.3.5"  # x-release-please-version
 
 __all__ = [
     "AbstractGimdowBLEDeviceManager",
