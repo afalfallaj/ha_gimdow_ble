e78b91044e2db593175629c817780690fc9efe2a
Abdullah Alfallaj <afalfallaj@gmail.com>
2026-07-25 15:45:05 +0300

    feat: import callback from homeassistant.core in select.py

--- custom_components/gimdow_ble/select.py
+++ custom_components/gimdow_ble/select.py
@@ -11,7 +11,7 @@ from homeassistant.components.select import (
     SelectEntityDescription,
     SelectEntity,
 )
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
